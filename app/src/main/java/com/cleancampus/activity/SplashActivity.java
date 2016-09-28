890d6cb461b28cbcba932df0e8c01ac3d95dc221
kbhardwj <kaushalbhardwaj12@gmail.com>
2016-09-28 19:09:26 +0530

    merged repo

--- app/src/main/java/com/cleancampus/activity/SplashActivity.java
+++ app/src/main/java/com/cleancampus/activity/SplashActivity.java
@@ -32,7 +32,7 @@ public class SplashActivity extends AppCompatActivity {
                 } catch (Exception e) {
                     e.printStackTrace();
                 } finally {
-                    Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                    Intent mainIntent = new Intent(SplashActivity.this, LoginActivity.class);
                     startActivity(mainIntent);
                     finish();
                 }
