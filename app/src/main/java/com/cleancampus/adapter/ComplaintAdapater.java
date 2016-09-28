620879ebaf60f8d637bc96ce2add0922d1fc023b
Chanpreet08 <chanpreet.chhabra@gmail.com>
2016-09-28 17:36:44 +0530

    login screen

--- app/src/main/java/com/cleancampus/adapter/ComplaintAdapater.java
+++ app/src/main/java/com/cleancampus/adapter/ComplaintAdapater.java
@@ -1,16 +1,25 @@
 package com.cleancampus.adapter;
 
+import android.content.Context;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.cleancampus.R;
+import com.cleancampus.fragment.Complaint;
 
 /**
  * Created by Chanpreet on 27-09-2016.
  */
-public class MotivationAdapter extends RecyclerView.Adapter {
+public class ComplaintAdapater extends RecyclerView.Adapter {
 
-
+    private Context context;
+    ComplaintAdapater(Context context1)
+    {
+        context = context1;
+    }
     @Override
     public int getItemCount() {
         return 0;
@@ -18,7 +27,9 @@ public class MotivationAdapter extends RecyclerView.Adapter {
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.row_layout_compliant,parent,false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
     }
 
     @Override
