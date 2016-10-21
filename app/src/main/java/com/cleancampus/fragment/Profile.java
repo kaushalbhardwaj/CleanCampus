f8ac415c42b354faa4df2b1806d4982b45dd7025
kbhardwj <kaushalbhardwaj12@gmail.com>
2016-10-21 17:43:24 +0530

    dustbin location

--- app/src/main/java/com/cleancampus/fragment/Profile.java
+++ app/src/main/java/com/cleancampus/fragment/Profile.java
@@ -1,8 +1,10 @@
 package com.cleancampus.fragment;
 
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.AppCompatImageView;
 import android.view.LayoutInflater;
@@ -23,6 +25,7 @@ public class Profile extends Fragment {
     private TextView signout;
     AppCompatImageView log;
     TextView userName,email;
+    FloatingActionButton fabDust;
 
 
     public Profile() {
@@ -39,6 +42,7 @@ public class Profile extends Fragment {
         log=(AppCompatImageView)v.findViewById(R.id.logout);
         userName=(TextView)v.findViewById(R.id.usernameprofile);
         email=(TextView)v.findViewById(R.id.textView2);
+        fabDust=(FloatingActionButton)v.findViewById(R.id.fabDust);
         UserInfo u= SharedPreference.getSharedPreferInfo(getActivity().getApplicationContext());
         userName.setText(u.getUserName());
         email.setText(u.getEmailId());
@@ -65,6 +69,14 @@ public class Profile extends Fragment {
                 Intent i=new Intent(getActivity().getApplicationContext(), LoginActivity.class);
                 startActivity(i);
                 getActivity().finish();
+            }
+        });
+        fabDust.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog=new Dialog(getActivity(),android.R.style.Theme_Black_NoTitleBar_Fullscreen);
+                dialog.setContentView(R.layout.layout_dustbin_map);
+                dialog.show();
             }
         });
 
